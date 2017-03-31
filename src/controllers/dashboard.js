98e9cb96415de74bc6feea91b53aa9fe60cc2100
tswayne <swaynetyler@gmail.com>
2017-03-31 11:39:18 -0700

    limit results

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,8 +1,8 @@
 
 module.exports.index = (req, res) => {
   const db = require('../../config/db').db;
-  db.collection('scores').find({user: req.user.id}).toArray((err, scores) => {
-    const App = req.renderComponent('App', {scores})
+  db.collection('scores').find({user: req.user.id}).limit( 150 ).sort({timestamp: -1}).toArray((err, scores) => {
+    const App = req.renderComponent('App', {scores: scores.reverse()})
     res.render('app', {app: App})
   })
 }
